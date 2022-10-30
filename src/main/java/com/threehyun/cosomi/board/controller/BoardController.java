a3c978b3ab92a416586fbb97991bfa6b3b88d6fb
DevHyundo <08hyundo@gmail.com>
2022-10-30 21:24:17 +0900

    feat board test page

--- src/main/java/com/threehyun/cosomi/board/controller/BoardController.java
+++ src/main/java/com/threehyun/cosomi/board/controller/BoardController.java
@@ -1,7 +1,13 @@
 package com.threehyun.cosomi.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
+
+    @GetMapping("/board")
+    public String getBoardMainPage(){
+        return "/group/board/board.html";
+    }
 }
