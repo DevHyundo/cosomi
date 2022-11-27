c6b0de937a18a4c872fda3b32f9a4139b2781f7b
hjseo <hjseo@mindpro.co.kr>
2022-11-27 18:18:22 +0900

    fix:회원가입기능

--- src/test/java/com/threehyun/cosomi/login/service/SignUpServiceImpleTest.java
+++ src/test/java/com/threehyun/cosomi/login/service/SignUpServiceImpleTest.java
@@ -1,5 +1,6 @@
 package com.threehyun.cosomi.login.service;
 
+import com.threehyun.cosomi.signUp.controller.dto.SignUpUserDto;
 import com.threehyun.cosomi.signUp.dao.SignUpMapper;
 import com.threehyun.cosomi.signUp.service.SignUpService;
 import org.assertj.core.api.Assertions;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Date;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -35,7 +37,42 @@ public class SignUpServiceImpleTest {
     @Test
     @DisplayName("회원가입")
     void signUpProcess(){
+        //given
+        String userEmail     = "test@naver.com";
+        String userPwd       = "1234";
+        String userName      = "테스트";
+        String userPhone     = "01055555555";
+        String userSocial    = "Home";
+        String userState     = "활성";
+
+        //when
+        SignUpUserDto sd = new SignUpUserDto();
+        sd.setUserEmail(userEmail);
+        sd.setUserPwd(userPwd);
+        sd.setUserName(userName);
+        sd.setUserPhone(userPhone);
+        sd.setUserSocial(userSocial);
+        sd.setUserState(userState);
+
+        //then
+        int i = signUpService.userAccountIns(sd);
+
+        Assertions.assertThat(i).isEqualTo(1);
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
